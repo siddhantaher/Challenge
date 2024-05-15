f42ea6868619c311f14ebaf7ec992724715be01a
siddhantaher <ahersiddhant7@gmail.com>
2024-05-14 23:18:00 -0700

    updated file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
+    
     <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</meta>
       <Navbar />
       <div className="container">
         <Routes>
